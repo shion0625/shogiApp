02cb12e33bd87fc81bb581e343cff1a7df43030f
shion0625 <xkaito0912@gmail.com>
2023-07-17 17:06:30 +0900

    新規ボードの使用

--- engine/main.go
+++ engine/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/shion0625/shogiApp/engine/src/board"
+	// "github.com/shion0625/shogiApp/engine/src/domain/model"
+)
+
 func main() {
+	sfen := "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1"
+	board, err := board.SetStartingPositionFromSFEN(sfen)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
+	// ビットボードを使った処理などを行うことができます
+	fmt.Println(board.Occupied)
 }
-
